f288373e86742746fed0d8c44b3b2cf27203f153
Andres Baeza <82929516+andyinho@users.noreply.github.com>
2026-05-03 23:35:24 -0700

    feat(seed): Add clinics and users with granular RBAC roles

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -2,7 +2,7 @@ import { prisma } from '../src/db/prisma.js';
 
 const main = async () => {
     try {
-        const clinic = await prisma.clinic.upsert({
+        const clinicA = await prisma.clinic.upsert({
             where: {
                 name: 'CareConnect Mobile',
             },
@@ -12,25 +12,91 @@ const main = async () => {
             },
         });
 
-        const staff = await prisma.user.upsert({
+        const clinicB = await prisma.clinic.upsert({
             where: {
-                email: 'andres@careconnect.care',
+                name: 'CareConnect San Francisco',
             },
             update: {},
             create: {
-                clinicId: clinic.id,
-                email: 'andres@careconnect.care',
-                role: 'STAFF',
+                name: 'CareConnect San Francisco',
             },
         });
 
-        console.log('Seed Connected!');
-        console.log(clinic, staff);
+        const staffA = await prisma.user.upsert({
+            where: {
+                email: 'andres@careconnect.care',
+            },
+            update: {
+                clinicId: clinicA.id,
+                role: 'PROVIDER',
+            },
+            create: {
+                clinicId: clinicA.id,
+                email: 'andres@careconnect.care',
+                role: 'PROVIDER',
+            },
+        });
+
+        const staffB = await prisma.user.upsert({
+            where: {
+                email: 'rebecca@careconnect.care',
+            },
+            update: {
+                clinicId: clinicA.id,
+                role: 'FRONT_DESK',
+            },
+            create: {
+                clinicId: clinicA.id,
+                email: 'rebecca@careconnect.care',
+                role: 'FRONT_DESK',
+            },
+        });
+
+        const staffC = await prisma.user.upsert({
+            where: {
+                email: 'steve@careconnect.care',
+            },
+            update: {
+                clinicId: clinicB.id,
+                role: 'FRONT_DESK',
+            },
+            create: {
+                clinicId: clinicB.id,
+                email: 'steve@careconnect.care',
+                role: 'FRONT_DESK',
+            },
+        });
+
+        const staffD = await prisma.user.upsert({
+            where: {
+                email: 'samantha@careconnect.care',
+            },
+            update: {
+                clinicId: clinicB.id,
+                role: 'ADMINISTRATIVE',
+            },
+            create: {
+                clinicId: clinicB.id,
+                email: 'samantha@careconnect.care',
+                role: 'ADMINISTRATIVE',
+            },
+        });
+
+        console.log('Seed connected!');
+        console.log({
+            clinicA,
+            staffA,
+            staffB,
+            clinicB,
+            staffC,
+            staffD,
+        });
     } catch (error) {
-        console.error('Seed Failed', error);
+        console.error('Seed failed', error);
         process.exit(1);
     } finally {
         await prisma.$disconnect();
     }
 };
+
 main();
